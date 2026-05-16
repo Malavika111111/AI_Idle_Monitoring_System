b8d6b413efabc30713a6d1e9240386cda8deec65
Malavika Suresh <malavikasuresh863@gmail.com>
2026-05-16 09:52:47 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+# ========================= DATABASE CONFIG =========================
+# Edit only this file to change your PostgreSQL connection settings.
+
+DB_HOST     = "pg-22d5507b-malavikasuresh863-dd36.h.aivencloud.com"
+DB_NAME     = "sample"
+DB_USER     = "avnadmin"
+DB_PASSWORD = "AVNS_p3l0LzK9loM4_BU32Bb"
+DB_PORT     = "24914"
